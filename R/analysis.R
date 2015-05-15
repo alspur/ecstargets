8aba92ef6af263b883a3f7c9b385769b7ab22d4e
Alex Spurrier <alex.spurrier@gmail.com>
2015-05-15 07:29:57 -0400

    update data and README to include approps budget

--- R/analysis.R
+++ R/analysis.R
@@ -4,30 +4,57 @@ library(ggmap)
 library(maptools)
 library(RColorBrewer)
 library(scales)
+library(readr)
 
 # load data
-ecs.data <- read.csv("data/ecs15revTarget.csv", stringsAsFactors = FALSE)
+ctvoices_data <- read_csv("data/ctvoicesECS.csv")
+alliance_list <- read_csv("data/allianceDistricts.csv")
+resident_data <- read_csv("data/resStudents.csv")
 
-# clean names
-ecs.data$town.name <- tolower(ecs.data$town.name)
+# clean alliance town names
 
-ecs.data$town.name <- gsub("(^|[[:space:]])([[:alpha:]])",
+colnames(alliance_list) <- c("town_id", "town_name", "alliance_status")
+
+alliance_list$town_name <- tolower(alliance_list$town_name)
+
+alliance_list$town_name <- gsub("(^|[[:space:]])([[:alpha:]])",
                            "\\1\\U\\2", 
-                           ecs.data$town.name, 
+                           alliance_list$town_name, 
                            perl=TRUE)
 
+# clean resident data  names
+
+colnames(resident_data)[1:2] <- c("town_id", "town_name")
+
+resident_data$town_name <- tolower(resident_data$town_name)
+
+resident_data$town_name <- gsub("(^|[[:space:]])([[:alpha:]])",
+                                "\\1\\U\\2", 
+                                resident_data$town_name, 
+                                perl=TRUE)
+
 # calculate summary data
-ecs.summary <- ecs.data %>%
-    summarise(ecs.fy15 = sum(fy15),
-              ecs.target = sum(as.numeric(rev.ecs.target)))%>%
-    mutate(ratio = ecs.fy15 / ecs.target)
+ecs_summary <- ctvoices_data %>%
+    summarise(ecs_fy16 = sum(ecs_fy16),
+              ecs_target = sum(as.numeric(fully_funded_target))) %>%
+    mutate(ratio = ecs_fy16 / ecs_target)
 
 # caculate grants based on target distribution @ current funding level
-ecs.data <- ecs.data %>%
-    mutate(ecs.15target = rev.ecs.target * ecs.summary$ratio,
-           change.15target =  ecs.15target - fy15,
-           diff.15target =  fy15 - ecs.15target,
-           pct.15target = fy15/ecs.15target)
+ecs_data <- ctvoices_data %>%
+    mutate(ecs_adj_target = fully_funded_target * ecs_summary$ratio,
+           change_fy16_target =  ecs_adj_target - ecs_fy16,
+           diff_fy16_target =  ecs_fy16 - ecs_adj_target,
+           pct_fy16_target = ecs_fy16 / ecs_adj_target)
+
+# join ecs data and alliance list
+
+ecs_data <- ecs_data %>%
+    left_join(alliance_list)
+
+# join ecs data and resident student data
+
+ecs_data <- ecs_data %>%
+    left_join(resident_data)
 
 # load shapefile
 towns <- readShapePoly("data/townct_37800_0000_2010_s100_census_1_shp/wgs84/townct_37800_0000_2010_s100_census_1_shp_wgs84.shp")
@@ -35,26 +62,26 @@ towns <- readShapePoly("data/townct_37800_0000_2010_s100_census_1_shp/wgs84/town
 
 # convert shapefile to dataframe
 
-town.geom <- fortify(towns)
+town_geom <- fortify(towns)
 
-town.geom$id <- as.integer(town.geom$id)
+town_geom$id <- as.integer(town_geom$id)
 
 # load map name key
 
-map.key <- data.frame(id = 0:172, town.name = towns@data$NAME10)
+map_key <- data.frame(id = 0:172, town_name = towns@data$NAME10)
 
 # join geom and key info
 
-town.map <- left_join(town.geom, map.key)
+town_map <- left_join(town_geom, map_key)
 
-town.map$town.name <- as.character(town.map$town.name)
+town_map$town_name <- as.character(town_map$town_name)
 
-town.data <- left_join(town.map, ecs.data, by = "town.name")
+town_data <- left_join(town_map, ecs_data, by = "town_name")
 
 
-#plot ecs vs adjusted fy15 ecs
-ggplot(town.data,
-       aes(x=long, y = lat, group = group, fill = diff.15target))+
+#plot ecs vs adjusted fy16 ecs
+ggplot(town_data,
+       aes(x=long, y = lat, group = group, fill = diff_fy16_target))+
     geom_polygon()+
     scale_fill_gradient2("Difference",
                          low = "firebrick4",
@@ -63,7 +90,7 @@ ggplot(town.data,
                          na.value = "grey54",
                          labels = dollar)+
     labs(x="",y="")+
-    ggtitle("Difference between FY15 ECS and Adjusted FY15 ECS")+
+    ggtitle("Difference between FY16 ECS and Adjusted FY16 ECS")+
     theme(panel.background = element_rect(fill = 'grey54', colour = 'grey54'),
           panel.grid.major = element_blank(), 
           panel.grid.minor = element_blank(),
@@ -77,18 +104,17 @@ ggplot(town.data,
           legend.position = "right")
 ggsave("figures/ecsDiff.png", width = 8, height = 5, unit = "in", dpi = 300)
 
-#plot percetage of fy15 ecs vs adjusted fy15 ecs
-ggplot(town.data,
-       aes(x=long, y = lat, group = group, fill = pct.15target))+
+#plot percetage of fy16 ecs vs adjusted fy16 ecs
+ggplot(town_data,
+       aes(x=long, y = lat, group = group, fill = pct_fy16_target))+
     geom_polygon()+
-    scale_fill_gradient2("FY15 ECS as\nPercentage of\nAdj. FY15 ECS",
-                         low = "firebrick4",
-                         high = "forestgreen", 
-                         midpoint = 1,
-                         na.value = "grey54",
-                         labels = percent)+
+    scale_fill_gradientn(colours = c("firebrick4","firebrick2", "white", "palegreen3", "darkgreen"),
+                         values = c(0.000, 0.060, 0.095, 0.105, 0.2, 1.000),
+                         breaks = c(0, 1, 4, 8),
+                         limits = c(0,10),
+                         na.value = "grey54")+
     labs(x="",y="")+
-    ggtitle("FY15 ECS as % of Adjusted FY15 ECS")+
+    ggtitle("FY16 ECS as % of Adjusted FY16 ECS")+
     theme(panel.background = element_rect(fill = 'grey54', colour = 'grey54'),
           panel.grid.major = element_blank(), 
           panel.grid.minor = element_blank(),
--- README.md
+++ README.md
@@ -5,4 +5,6 @@ Comparison of current ECS grants to target amounts.
 
 FY15 ECS Grants obtained from [Connecticut Public Act 14-47, Section 18](http://www.cga.ct.gov/2014/ACT/PA/2014PA-00047-R00HB-05596-PA.htm).
 
-ECS Fully-Funded Target amounts were obtained from a handout at an ECS Task Force meeting and adjusted to prevent any town's fully-funded ECS amount from being lower than their FY15 ECS funding. 
+ECS Fully-Funded Target amounts were obtained from [testimony presented by Kenneth Feder and Rachel Leventhal-Weiner on behalf of Connecticut Voices for Children](http://www.ctvoices.org/sites/default/files/040215_approps_sb816_ecsmin50pct.pdf).
+
+The Appropriations Committee ECS budget for FY16 and FY17 was obtained from the text of [HB6824, File No. 800](http://www.cga.ct.gov/2015/FC/2015HB-06824-R000800-FC.htm).
